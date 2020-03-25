e5494776a946e3185d4a0be63b5903cdddd5ca9f
Yuk, jeonggeun <wjdrms5815@naver.com>
2020-03-25 23:10:29 +0900

    initial commit

---
+++ hw/pages.js
@@ -0,0 +1,4 @@
+let studentCount=31
+let pagesize=10
+
+console.log(Math.floor(studentCount/pagesize))
---
+++ hw/random.js
@@ -0,0 +1,3 @@
+for (let i = 0; i <= 30; i++) {
+   let a = Math.floor(Math.random() * 11 + 10);
+   console.log(a); }
